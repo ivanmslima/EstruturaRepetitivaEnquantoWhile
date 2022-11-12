91c684b34964a62c3012f6868846ae1dfb44a98f
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-11 22:08:54 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaRepetitivaEnquantoWhile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args) {
+
+            Console.WriteLine("       WHILE é recomendada quando não se sabe previamente a quantidade de repetições ");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Digite um numero, quando o número 0 for digitado será somado todos os números ");
+
+            int x, soma;
+
+            x = int.Parse(Console.ReadLine());
+            soma = 0;
+            while (x != 0)
+            {
+                soma = soma + x;
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("A soma de todos os números digitados é: " + soma);
+            Console.ReadLine();
+           
+        }
+    }
+}
